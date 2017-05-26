a80d0badff3de85038be99fceac3da43d21de441
Slagtand <mrc152691@gmail.com>
2017-05-26 08:50:13 +0200

    Update permisos_others.py

--- Permisos26-05-17/permisos_others.py
+++ Permisos26-05-17/permisos_others.py
@@ -4,27 +4,23 @@ import os
 import stat
 
 path_to_explore="./Documents/"
+permissions1=0
+permissions2=0
 
 #####################################################################
 # Mostrem ruta tot
-def comprobacio_perm (path_to_explore):
-    for root, dirs, files in os.walk(path_to_explore):
-        for name in files:
-            name_path=os.path.join(root, name)
-            print(name_path) ,
-            permissions1 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
-            return permissions1
+for root, dirs, files in os.walk(path_to_explore):
+    for name in files:
+        name_path=os.path.join(root, name)
+        permissions1 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
+    for name in dirs:
+        name_path=os.path.join(root, name)
+        permissions2 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
 
-
-        for name in dirs:
-            name_path=os.path.join(root, name)
-            print(name_path) ,
-            permissions2 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
-            return permissions2
 
 
 ############################################################
-if (comprobacio_perm[-1]!= 0):
+if ((permissions1[-1]!= 0) or (permissions2[-1]!= 0)):
     print name_path
     print "Atenció!"
 else:
