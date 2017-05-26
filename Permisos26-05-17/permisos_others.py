843444e0516ad4935542a165342df29091abeb84
Slagtand <mrc152691@gmail.com>
2017-05-26 09:11:17 +0200

    Update permisos_others.py

--- Permisos26-05-17/permisos_others.py
+++ Permisos26-05-17/permisos_others.py
@@ -8,18 +8,21 @@ permissions1=0
 permissions2=0
 
 #####################################################################
-# Mostrem ruta tot
+# Fem un for per a que recorri cada carpeta i fitxer recursivament.
 for root, dirs, files in os.walk(path_to_explore):
     for name in files:
         name_path=os.path.join(root, name)
+        # Treiem els permisos
         permissions1 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
     for name in dirs:
         name_path=os.path.join(root, name)
+        # Treiem els permisos
         permissions2 = oct(stat.S_IMODE ( os.stat (name_path).st_mode ))
 
 
 
 ############################################################
+# Comparem els resultats dels permisos de others amb el [-1], si és diferent a 0 salta la alarma
 if ((permissions1[-1]!= 0) or (permissions2[-1]!= 0)):
     print name_path
     print "Atenció!"
