af3e543e8041a2e92c53f6b73ff319cd140c0405
Slagtand <mrc152691@gmail.com>
2017-05-31 19:15:37 +0000

    Update tamany.py

--- Exercicis01-06-17/tamany.py
+++ Exercicis01-06-17/tamany.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import os
- 
+#Indiquem la ruta que volem explorar 
 path_to_explore="./Documents/"
 total_size=0
         
@@ -8,8 +8,9 @@ total_size=0
 for root, dirs, files in os.walk(path_to_explore):
     for name in files:
         name_path=os.path.join(root, name)
+	#Mostrem el tamany de cada arxiu
         tamany=os.stat(name_path).st_size
         
-
+#Comparem si l'arxiu és supeior o igual a 1 GB (2³⁰ B son 1 GB)
 if (tamany>=2**30):
 	print "Els arxius que ocupen més d'1GB són:\n", name_path , tamany
